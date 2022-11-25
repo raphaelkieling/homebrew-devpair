e5d08b4e8f78099176e449e2e23d4013290f8d2d
Raphael Kieling <rapha_kieling@hotmail.com>
2022-11-24 21:45:11 -0300

    Update devpair.rb

--- devpair.rb
+++ devpair.rb
@@ -4,7 +4,7 @@ class Devpair < Formula
   desc "Tool to make the pair programming easier"
   homepage "https://github.com/raphaelkieling/devpair"
   head "https://github.com/raphaelkieling/devpair"
-  url "https://github.com/raphaelkieling/devpair/archive/refs/tags/v1.1.16.tar.gz"
+  url "https://github.com/raphaelkieling/devpair/archive/refs/tags/v1.1.18.tar.gz"
   depends_on "python@3.10"
 
   resource "click" do
